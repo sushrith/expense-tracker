fc1a3823103895a44791d156b047ce18178dd486
Manas Sinkar <manas.sinkar@gmail.com>
2023-07-14 17:11:08 +0530

    Fixes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React from "react";
 import styled from "styled-components";
-import HomeComponent from "./modules/home";
-
+import axios from 'axios';
+import React, { useEffect, useState } from "react";
 const Container = styled.div`
   background-color: white;
   color: #0d1d2c;
@@ -15,6 +14,18 @@ const Container = styled.div`
   font-family: Montserrat;
 `;
 
+const Container1 = styled.div`
+  background-color: white;
+  color: #0d1d2c;
+  display: flex;
+  flex-direction: column;
+  padding: 10px 22px;
+  font-size: 18px;
+  width: 360px;
+  align-items: center;
+  justify-content: space-between;
+`;
+
 const Header = styled.div`
   background-color: white;
   color: #0d1d2c;
@@ -25,7 +36,266 @@ const Header = styled.div`
   font-size: 25px;
   font-weight: bold;
 `;
-const App = () => {
+
+
+const HomeComponent = (props) => {
+    const [transactions, updateTransaction] = useState([]);
+    const [expense, updateExpense] = useState(0);
+    // const [income, updateIncome] = useState(0);
+
+    const calculateBalance = () => {
+        let exp = 0;
+        transactions.map((payload) =>
+            exp=exp+payload.amt
+        );
+        updateExpense(exp);
+    };
+    useEffect(() => calculateBalance(), [transactions]);
+    useEffect(() => {
+        const transactionArray = [...transactions];
+        axios.get(`http://127.0.0.1:5000/api/users/${1}/expenses`).then(res=>{
+            console.log(res);
+            res.data.map(val=>{
+                let item = {amt:val.amt,desc:val.desc,category:val.category}
+                transactionArray.push(item);
+            });
+            updateTransaction(transactionArray);
+        })
+    },[])
+    const addTransaction = (payload) => {
+        const transactionArray = [...transactions];
+        // fetch()
+        axios.post(`http://127.0.0.1:5000/api/users/${1}/expenses`,{
+            desc:payload.desc,
+            amt:payload.amt,
+            category:payload.category
+        }).then((res)=>{console.log(res)});
+        transactionArray.push(payload);
+        updateTransaction(transactionArray);
+    };
+    return (
+        <Container1>
+            <OverViewComponent
+                expense={expense}
+                // income={income}
+                addTransaction={addTransaction}
+            />
+            {transactions?.length ? (
+                <TransactionsComponent transactions={transactions} />
+            ) : (
+                ""
+            )}
+        </Container1>
+    );
+};
+
+const Container2 = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 10px;
+  align-items: center;
+  font-size: 16px;
+  width: 100%;
+`;
+const BalanceBox = styled.div`
+  font-size: 18px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  font-weight: bold;
+  & span {
+    color: #0d1d2c;
+    opacity: 80%;
+    font-weight: bold;
+    font-size: 20px;
+  }
+`;
+const AddTransaction =styled.div`
+  font-size: 15px;
+  background: #0d1d2c;
+  display: flex;
+  color: white;
+  padding: 5px 10px;
+  cursor: pointer;
+  flex-direction: row;
+  border-radius: 4px;
+  font-weight: bold;
+`;
+const AddTransactionContainer = styled.div`
+  font-size: 15px;
+  display: ${(props) => (props.isAddTxnVisible ? "flex" : "none")};
+  color: #0d1d2c;
+  flex-direction: column;
+  border-radius: 4px;
+  border: 1px solid #e6e8e9;
+  width: 100%;
+  align-items: center;
+  padding: 15px 20px;
+  margin: 10px 20px;
+  gap: 10px;
+  & input {
+    width: 90%;
+    outline: none;
+    padding: 10px 12px;
+    border-radius: 4px;
+    border: 1px solid #e6e8e9;
+  }
+`;
+
+const AddTransactionView = (props) => {
+  const [amt, setAmt] = useState();
+  const [desc, setDesc] = useState();
+  const [category, setCategory] = useState();
+  // const [type, setType] = useState("EXPENSE");
+  const [check,setCheck]=useState(false);
+
+  useEffect(()=>{
+    if(!amt)
+    {
+      setCheck(false);
+    }
+    else{
+      setCheck(true);
+    }
+
+  },[amt]);
+  return (
+    <AddTransactionContainer isAddTxnVisible={props.isAddTxnVisible}>
+      <input
+        placeholder="Amount"
+        type="number"
+        value={amt}
+        onChange={(e) => setAmt(e.target.value)}
+        />
+      <input
+        placeholder="Description"
+        value={desc}
+        onChange={(e) => setDesc(e.target.value)}
+      />
+      <input
+        placeholder="Category"
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
+      />
+
+      {check &&<AddTransaction
+        onClick={() =>
+          props.addTransaction({
+            id: Date.now(),
+            amt: Number(amt),
+            desc,
+            category,
+          })
+        }
+      >
+        Add Transaction
+      </AddTransaction>}
+    </AddTransactionContainer>
+  );
+};
+const OverViewComponent = (props) => {
+  const [isAddTxnVisible, toggleAddTXn] = useState(false);
+  return (
+    <Container2>
+      <BalanceBox>
+        Expense: ${props.expense}
+        <AddTransaction onClick={() => toggleAddTXn((isVisible) => !isVisible)}>
+          {isAddTxnVisible ? "CANCEL" : "ADD"}
+        </AddTransaction>
+      </BalanceBox>
+      {isAddTxnVisible && (
+        <AddTransactionView
+          isAddTxnVisible={isAddTxnVisible}
+          addTransaction={(payload) => {
+            props.addTransaction(payload);
+            toggleAddTXn((isVisible) => !isVisible);
+          }}
+        />
+      )}
+    </Container2>
+  );
+};
+const Container3 = styled.div`
+  background-color: white;
+  color: #0d1d2c;
+  display: flex;
+  flex-direction: column;
+  padding: 10px 22px;
+  font-size: 18px;
+  width: 100%;
+  gap: 10px;
+  font-weight: bold;
+  overflow-y: auto !important;
+  & input {
+    padding: 10px 12px;
+    border-radius: 12px;
+    background: #e6e8e9;
+    border: 1px solid #e6e8e9;
+    outline: none;
+  }
+`;
+const Cell = styled.div`
+  background-color: white;
+  color: #0d1d2c;
+  display: flex;
+  flex-direction: row;
+  padding: 10px 15px;
+  font-size: 14px;
+  border-radius: 2px;
+  border: 1px solid #e6e8e9;
+  align-items: center;
+  font-weight: normal;
+  justify-content: space-between;
+  border-right: 4px solid ${(props) => (props.isExpense ? "red" : "green")};
+`;
+const TransactionCell = (props) => {
+  return (
+    <Cell isExpense="true">
+      <span>{props.payload?.category}</span>
+      <span>{props.payload?.desc}</span>
+      <span>${props.payload?.amt}</span>
+    </Cell>
+  );
+};
+const TransactionsComponent = (props) => {
+  const [searchText, updateSearchText] = useState("");
+  const [filteredTransaction, updateTxn] = useState(props.transactions);
+
+  const filterData = (searchText) => {
+    if (!searchText || !searchText.trim().length) {
+      updateTxn(props.transactions);
+      return;
+    }
+    let txn = [...props.transactions];
+    txn = txn.filter((payload) =>
+      payload.desc.toLowerCase().includes(searchText.toLowerCase().trim()),
+    );
+    updateTxn(txn);
+  };
+
+  useEffect(() => {
+    filterData(searchText);
+  }, [props.transactions]);
+
+  return (
+    <Container3>
+      Transactions
+      <input
+        placeholder="Search"
+        onChange={(e) => {
+          updateSearchText(e.target.value);
+          filterData(e.target.value);
+        }}
+      />
+      {filteredTransaction?.map((payload) => (
+        <TransactionCell payload={payload} />
+      ))}
+    </Container3>
+  );
+};
+const App2 = () => {
   return (
     <Container>
       <Header>Expense Tracker</Header>
@@ -34,4 +304,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default App2;
---
+++ src/app.py
@@ -0,0 +1,111 @@
+import json
+from datetime import datetime
+from typing import Dict, List
+
+from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+filepath = "data.json"
+
+
+class Expense:
+    def __init__(self, desc: str, amt: float, category: str, timestamp: datetime) -> None:
+        self.desc = desc
+        self.amt = amt
+        self.category = category
+        self.timestamp = timestamp
+
+    def to_dict(self) -> Dict:
+        return {
+            "desc": self.desc,
+            "amt": self.amt,
+            "category": self.category,
+            "timestamp": self.timestamp#.strftime("%Y-%m-%d %H:%M:%S"),
+        }
+
+
+class User:
+    def __init__(self, username: str, expenses: List[Expense]) -> None:
+        self.username = username
+        self.expenses = expenses
+
+    def to_dict(self) -> Dict:
+        return {
+            "username": self.username,
+            "expenses": [expense.to_dict() for expense in self.expenses],
+        }
+
+
+def load_data() -> Dict:
+    try:
+        with open(filepath, "r") as f:
+            return json.load(f)
+    except:
+        return {}
+
+
+def save_data(data: Dict) -> None:
+    with open(filepath, "w") as f:
+        print('dumping te data')
+        json.dump(data, f)
+    print("Dumped")
+
+
+def add_expense(username: str, desc: str, amt: float, category: str, timestamp: datetime) -> str:
+    print("Adiding inside ad_expsen")
+    data = load_data()
+    if username not in data:
+        data[username] = {"expenses": []}
+
+    user = User(username, [Expense(desc, amt, category, timestamp)])
+    data[username]["expenses"].append(user.to_dict()["expenses"][0])
+    save_data(data)
+    return "Added expense successfully."
+
+
+def get_expenses(username: str) -> List[Expense]:
+    data = load_data()
+    if username in data:
+        user_dict = data[username]
+        expenses_dicts = user_dict.get("expenses", [])
+        return [Expense(**d) for d in expenses_dicts]
+    return []
+    
+
+def get_expenses_by_category(username: str, category: str) -> List[Expense]:
+    expenses = get_expenses(username)
+    return [expense for expense in expenses if expense.category == category]
+
+
+@app.route("/api/users/<int:user_id>/expenses", methods=["GET", "POST"])
+@cross_origin()
+def expenses(user_id: int):
+    print("Expense API called")
+    if request.method == "GET":
+        expenses = get_expenses(f"user{user_id}")
+        return jsonify([expense.to_dict() for expense in expenses])
+    elif request.method == "POST":
+        print("adding useer")
+        data = request.get_json()
+        desc = data.get("desc", "")
+        amt = data.get("amt", 0.0)
+        category = data.get("category", "")
+        timestamp = str(datetime.now())
+        result = add_expense(f"user{user_id}", desc, amt, category, timestamp)
+        print("Added")
+        return jsonify({"result": result})
+
+
+
+
+@app.route("/")
+def hello():
+    return "Hello"
+from datetime import datetime
+if __name__ == "__main__":
+
+    app.run(debug=True)
